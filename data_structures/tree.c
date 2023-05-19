0827f43c7d4ae97d7f673a68cb146af6d2243e0a
RyuseiNa <nakayama_ryusei@retail-ai.jp>
2023-05-19 11:08:22 +0900

    やるべきデータ構造追加

---
+++ data_structures/tree.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct Node* right;
+    struct Node* left;
+}Node;
+
+Node* craeteNode(int data){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->right = NULL;
+    newNode->left = NULL;
+    return newNode;
+}
+Node* insertNode(Node* root,int data){
+    if(root==NULL){
+        craeteNode(data);
+    } else {
+        if(){
+
+        }else{
+            
+        }
+    }
+}
+
+void addNode(){
+
+}
+void removeNode(){
+
+}
